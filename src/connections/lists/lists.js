ba50fa9af3d642ffc534118486cbbe36c315ea28
ankostyuk <andrew.kostyuk@gmail.com>
2016-04-22 17:44:08 +0300

    Директива np-connections-lists-add-list-entries

--- src/connections/lists/lists.js
+++ src/connections/lists/lists.js
@@ -43,7 +43,7 @@ define(function(require, exports, module) {'use strict';
         .directive('npConnectionsLists', ['$log', 'npConnectionsListsResource', function($log, npConnectionsListsResource){
             return {
                 restrict: 'A',
-                scope: true,
+                scope: {},
                 template: viewTemplates['lists-view'].html,
                 link: function(scope, element, attrs) {
                     //
@@ -105,13 +105,88 @@ define(function(require, exports, module) {'use strict';
                     showLists();
 
                     //
+                    // new-list
+                    //
+                    var newList = {
+                        list: {
+                            name: null,
+                            type: 'COMPANY' // TODO null
+                        },
+                        isReady: function() {
+                            return newList.list.name && newList.list.type;
+                        },
+                        addListEntriesProxy: {
+                            getListType: function() {
+                                return newList.list.type;
+                            }
+                        }
+                    };
+
+                    //
                     _.extend(scope, {
                         navigation: navigation,
-                        lists: lists
+                        lists: lists,
+                        newList: newList
                     }, i18n.translateFuncs);
 
                     // test
                     showNav('#np-connections-lists-new-list');
+                }
+            };
+        }])
+        //
+        .directive('npConnectionsListsAddListEntries', ['$log', '$timeout', function($log, $timeout){
+            return {
+                restrict: 'A',
+                scope: {
+                    proxy: '=npConnectionsListsAddListEntries'
+                },
+                template: viewTemplates['add-list-entries-view'].html,
+                link: function(scope, element, attrs) {
+                    //
+                    var formElement = element.find('form'),
+                        fileElement = element.find('.add-file input'),
+                        textElement = element.find('textarea');
+
+                    //
+                    _.extend(scope, {
+                        target: null,
+                        file: null,
+                        doTarget: function(target) {
+                            if (target === 'text') {
+                                resetFile();
+                                
+                                $timeout(function(){
+                                    textElement.focus();
+                                });
+                            }
+                            scope.target = target;
+                        }
+                    });
+
+                    //
+                    fileElement.change(function(e){
+                        $timeout(function(){
+                            doFile();
+                        });
+                    });
+
+                    function doFile() {
+                        scope.file = _.get(fileElement.get(0), 'files[0]');
+
+                        $log.info('* file:', scope.file);
+
+                        if (scope.file) {
+                            // fileReader.readAsDataURL(file);
+
+                            scope.doTarget('file');
+                        }
+                    }
+
+                    function resetFile() {
+                        scope.file = null;
+                        formElement.get(0).reset();
+                    }
                 }
             };
         }]);
